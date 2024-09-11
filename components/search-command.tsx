2ff27fbf00b9d4287a6d68456c63b7536d0abd29
Deepanshu <deepanshumi200@gmail.com>
2024-09-11 17:19:23 +0530

    ??

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -17,7 +17,7 @@ import {
 import useSearch from "@/hooks/use-search";
 import { api } from "@/convex/_generated/api";
 
-const SearchCommand = () => {
+export const SearchCommand = ()=>{
     const {user} = useUser();
     const router = useRouter();
     const documents = useQuery(api.documents.getSearch);
@@ -33,54 +33,56 @@ const SearchCommand = () => {
 
     useEffect(()=>{
         const down = (e:KeyboardEvent)=>{
-            if (e.key === 'k'&& (e.metaKey || e.ctrlKey)){
+            if(e.key==="k" && (e.metaKey||e.ctrlKey)){
                 e.preventDefault();
                 toggle();
             }
         }
 
         document.addEventListener("keydown", down);
-        return () => document.removeEventListener("keydown", down);
-    }, [toggle]);
-
-    if(!isMounted){
-        return null;
-    }
+        return ()=> document.removeEventListener("keydown", down);
+    }, [toggle])
 
     const onSelect = (id:string)=>{
         router.push(`/documents/${id}`)
         onClose();
     };
 
-    return ( 
+    if(!isMounted){
+        return null;
+    }
+
+    return(
         <CommandDialog open={isOpen} onOpenChange={onClose}>
-            <CommandInput 
-                placeholder={`Search ${user?.fullName}'s Jotion`}/>
+            <CommandInput
+                placeholder={`Search ${user?.firstName}'s Motion...`}
+            />
             <CommandList>
-                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandEmpty>
+                    No results found.
+                </CommandEmpty>
+
                 <CommandGroup heading="Documents">
                     {documents?.map((document)=>(
                         <CommandItem
-                            key={document._id}
+                            key = {document._id}
                             value={`${document._id}-${document.title}`}
                             title={document.title}
-                            onSelect={onSelect}>
-                            {document.icon?(
-                                <p className="mr-2 text-[18px]">
-                                    {document.icon}
-                                </p>
-                            ):(
-                                <File className="mr-2 h-4 w-4"/>
-                            )}
-                            <span>
-                                {document.title}
-                            </span>
+                            onSelect={()=>onSelect(document._id)}
+                            >
+                                {document.icon?(
+                                    <p className="mr-2 text-[18px]">
+                                        {document.icon}</p>
+                                ):(
+                                    <File className="mr-2 h-4 w-4"/>
+                                )}
+                                <span>
+                                    {document.title}
+                                </span>
                         </CommandItem>
                     ))}
                 </CommandGroup>
             </CommandList>
         </CommandDialog>
-     );
+    )
 }
- 
-export default SearchCommand;
